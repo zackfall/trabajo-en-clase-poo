900acc3e56e216b1ee2d67e7109ecaa2ba629690
Isaac Zamora <zackfall@outlook.com>
2023-09-19 15:59:39 +0000

    feat: add a new instance

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { Coche } from "./coche";
 import { Motor } from "./motor";
 
 const motor = new Motor("A3WB");
-const coche = new Coche(5, "Volkswagen", "34EF", motor);
+const cocheVolkswagen = new Coche(5, "Volkswagen", "34EF", motor);
+console.log(cocheVolkswagen.imprimir());
 
-console.log(coche.imprimir());
+const cocheToyota = new Coche(4, "Toyota", "Yaris", motor);
+console.log(cocheToyota.imprimir());
